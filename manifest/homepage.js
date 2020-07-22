6d2e4333c556632eb4668f3cd9a9eaabb0e867fc
Max Webb <60947369+maxwebbnz@users.noreply.github.com>
2020-07-23 09:49:06 +1200

    v0.6 fix
    issues between branches

--- manifest/homepage.js
+++ manifest/homepage.js
@@ -1,110 +1,114 @@
 $( document ).ready(function() {
-    $( "#header" ).fadeIn( "20000", function() {});
-    document.title = "Welcome to Rob's Rental!"
+  $( "#header" ).fadeIn( "20000", function() {});
+  document.title = "Welcome to Rob's Rental!"
 });
 
 $( "beginbutton" ).click(function() {
-    alert( "Handler for .click() called." );
-  });
+  alert( "Handler for .click() called." );
+});
 
-  function hideHeader(){
-    $( "#header" ).fadeOut( "20000", function() {});
-    $( "#getname" ).fadeIn( "3000", function() {});
-    $('body').animate({backgroundColor: 'blue'}, 'slow');
-    document.title = "Your Name";
-  }
+function hideHeader(){
+  $( "#header" ).fadeOut( "20000", function() {});
+  $("#header").animate({marginRight: '1000px'}, "slow");
+  $("#getname").animate({marginLeft: '1%'}, "slow");
+  $( "#getname" ).fadeIn( "3000", function() {});
+  $('body').animate({backgroundColor: 'blue'}, 'slow');
+  document.title = "Your Name";
+}
 
-
-  function clientNameFinder(){
-    clientName = document.getElementById("clientname").value;
-    // if is a number don't allow, if it is null don't allow, if the box is empty or has spaces don't allow
-    if(!isNaN(clientName) || clientName == null || clientName == "" ){
-      alert("Whoops! You have entered in a value which is not what we want! Try again.")
-      // Common names that are entered as fake
-    }else if(clientName == commonNames){
-      // We want to make sure we have a real name
-      confirm("Are you sure your name is " + clientName + "?")
-      if(confirm == true){
-        // if they are actually weirdly called that name let them pass (hey no judgement here.)
-        hideNamePrompt()
-      }else if(confirm == false){
-        // Remind the client not to be dumb
-        alert("Enter in a real name and try again.")
-      }
-      // Silly me at 10:33PM trying to get my head around it not still sending it to the next step. fixed :>
-    }else{
-      hideNamePrompt();
+function clientNameFinder(){
+  clientName = document.getElementById("clientname").value;
+  // if is a number don't allow, if it is null don't allow, if the box is empty or has spaces don't allow
+  if(!isNaN(clientName) || clientName == null || clientName == "" ){
+    alert("Whoops! You have entered in a value which is not what we want! Try again.")
+    // Common names that are entered as fake
+  }else if(clientName == commonNames){
+    // We want to make sure we have a real name
+    confirm("Are you sure your name is " + clientName + "?")
+    if(confirm == true){
+      // if they are actually weirdly called that name let them pass (hey no judgement here.)
+      hideNamePrompt()
+    }else if(confirm == false){
+      // Remind the client not to be dumb
+      alert("Enter in a real name and try again.")
     }
-    console.log(clientName);
-    console.log("Function Completed without a refresh needed!")
+    // Silly me at 10:33PM trying to get my head around it not still sending it to the next step. fixed :>
+  }else{
+    hideNamePrompt();
   }
+  console.log(clientName);
+  console.log("Function Completed without a refresh needed!")
+}
 
-  function hideNamePrompt(){
-    $( "#getname" ).fadeOut( "20000", function() {});
-    $( "#seatneeded" ).fadeIn( "3000", function() {});
-    $('body').animate({backgroundColor: 'yellow'}, 'slow');
-    document.title =  "Seat Selector";
-    console.log(clientName);
-  }
+function hideNamePrompt(){
+  $( "#getname" ).fadeOut( "20000", function() {});
+  $("#seatneeded").animate({marginLeft: '1%'}, "slow");
+  $( "#seatneeded" ).fadeIn( "3000", function() {});
+  $('body').animate({backgroundColor: 'yellow'}, 'slow');
+  document.title =  "Seat Selector";
+  console.log(clientName);
+}
 
-  function seatsFinder(){
-      clientSeatSelc = document.getElementById("seatsneeded").value;
-      if(clientSeatSelc == null || clientSeatSelc == ""){
-        alert("Whoops! You have entered in a value which is not what we want! Try again.")
-      };
-      if(clientSeatSelc){
-        hideSeatsPrompt()
-      }  
-    }
-
-  function hideSeatsPrompt(){
-    $( "#seatneeded" ).fadeOut( "20000", function() {});
-    document.title = "Rental Dates"
-    $( "#setrentalleng-start" ).fadeIn( "3000", function() {});
-  }
-
-  function setRentalStartDate(){
-    startDate = document.getElementById("startDate").value;
-    if(startDate == null){
+function seatsFinder(){
+    clientSeatSelc = document.getElementById("seatsneeded").value;
+    if(clientSeatSelc == null || clientSeatSelc == ""){
       alert("Whoops! You have entered in a value which is not what we want! Try again.")
     };
-    if(startDate){
-      hideStartDate()
+    if(clientSeatSelc){
+      hideSeatsPrompt()
     }  
   }
 
-  function hideStartDate() {
-    $( "#setrentalleng-start" ).fadeOut( "20000", function() {});
-    $( "#setrentalleng-end" ).fadeIn( "3000", function() {});
-    // setRentalEndDate();
-  }
+function hideSeatsPrompt(){
+  $( "#seatneeded" ).fadeOut( "20000", function() {});
+  document.title = "Rental Dates"
+  $("#setrentalleng-start").animate({marginLeft: '1%'}, "slow");
+  $( "#setrentalleng-start" ).fadeIn( "3000", function() {});
+}
 
-  function setRentalEndDate(){
-    endDate = document.getElementById("endDate").value;
-    if(endDate == null || endDate == ""){
-      alert("Whoops! You have entered in a value which is not what we want! Try again.")
-    };
-    if(endDate){
+function setRentalStartDate(){
+  startDate = document.getElementById("startDate").value;
+  if(startDate == null || isNaN(startDate)){
+    alert("Whoops! You have entered in a value which is not what we want! Try again.")
+  };
+  if(startDate == true){
+    hideStartDate()
+  }  
+}
+
+function hideStartDate() {
+  $( "#setrentalleng-start" ).fadeOut( "20000", function() {});
+  $("#setrentalleng-end").animate({marginLeft: '1%'}, "slow");
+  $( "#setrentalleng-end" ).fadeIn( "3000", function() {});
+  // setRentalEndDate();
+}
+
+function setRentalEndDate(){
+  endDate = document.getElementById("endDate").value;
+  if(endDate == null || endDate == "" || isNaN(endDate)){
+    alert("Whoops! You have entered in a value which is not what we want! Try again.")
+  };
+  if(endDate){
       confirmClient()
-    }
   }
+}
 
-  function confirmClient(){
-    document.title = "Everything look good?";
-    $( "#setrentalleng-end" ).fadeOut( "20000", function() {});
-    $( "#clientconfirm" ).fadeIn( "3000", function() {});
-    document.getElementById("clname").innerHTML = clientName; 
-    document.getElementById("clientseats").innerHTML = clientSeatSelc; 
-    document.getElementById("clientrentaldays").innerHTML = endDate - startDate; 
-  }
+function confirmClient(){
+  document.title = "Everything look good?";
+  $( "#setrentalleng-end" ).fadeOut( "20000", function() {});
+  $("#clientconfirm").animate({marginLeft: '1%'}, "slow");
+  $( "#clientconfirm" ).fadeIn( "3000", function() {});
+  document.getElementById("clname").innerHTML = clientName; 
+  document.getElementById("clientseats").innerHTML = clientSeatSelc; 
+  document.getElementById("clientrentaldays").innerHTML = endDate - startDate; 
+}
 
-  function confirmed(){
-    document.title = "Processing....";
-    $( "#clientconfirm" ).fadeOut( "3000", function() {});
-    $('body').animate({backgroundColor: 'black'}, 'slow');
-    processInfomation();
-  }
-
+function confirmed(){
+  document.title = "Processing....";
+  $( "#clientconfirm" ).fadeOut( "3000", function() {});
+  $('body').animate({backgroundColor: 'black'}, 'slow');
+  processInfomation();
+}
   function processInfomation(){
     $( "#pleasewait" ).fadeIn( "3000", function() {});
 
@@ -163,4 +167,4 @@ $( "beginbutton" ).click(function() {
     document.getElementById("carimage").src = selectedcar.imgurl;
     document.getElementById("rentalprice").innerHTML = finalPrice;
 
-  }
+  } 
