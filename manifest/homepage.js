a2115f5e9829759fbbe5577595a2e7030f55cdf2
Max Webb <60947369+maxwebbnz@users.noreply.github.com>
2020-07-23 13:27:06 +1200

    (v0.6) additons and removals

--- manifest/homepage.js
+++ manifest/homepage.js
@@ -14,6 +14,11 @@ function hideHeader(){
   $( "#getname" ).fadeIn( "3000", function() {});
   $('body').animate({backgroundColor: 'blue'}, 'slow');
   document.title = "Your Name";
+  $( function() {
+    $( "#progressbar" ).progressbar({
+      value: 10
+    });
+  });
 }
 
 function clientNameFinder(){
@@ -47,60 +52,48 @@ function hideNamePrompt(){
   $('body').animate({backgroundColor: 'yellow'}, 'slow');
   document.title =  "Seat Selector";
   console.log(clientName);
+  $( function() {
+    $( "#progressbar" ).progressbar({
+      value: 20
+    });
+  });
 }
 
 function seatsFinder(){
     clientSeatSelc = document.getElementById("seatsneeded").value;
-    if(clientSeatSelc == null || clientSeatSelc == ""){
+    if(clientSeatSelc == null || clientSeatSelc == "" || isNaN(clientSeatSelc)){
       alert("Whoops! You have entered in a value which is not what we want! Try again.")
-    };
-    if(clientSeatSelc){
+    }else{
       hideSeatsPrompt()
     }  
   }
 
 function hideSeatsPrompt(){
   $( "#seatneeded" ).fadeOut( "20000", function() {});
-  document.title = "Rental Dates"
-  $("#setrentalleng-start").animate({marginLeft: '1%'}, "slow");
-  $( "#setrentalleng-start" ).fadeIn( "3000", function() {});
+  document.title = "Rental Time"
+  $("#setrentalleng").animate({marginLeft: '1%'}, "slow");
+  $( "#setrentalleng" ).fadeIn( "3000", function() {});
 }
 
-function setRentalStartDate(){
-  startDate = document.getElementById("startDate").value;
-  if(startDate == null || startDate == "" || isNaN(startDate)){
-    alert("Whoops! You have entered in a value which is not what we want! Try again.")
-  };
-  if(startDate == true){
-    hideStartDate()
-  }  
-}
-
-function hideStartDate() {
-  $( "#setrentalleng-start" ).fadeOut( "20000", function() {});
-  $("#setrentalleng-end").animate({marginLeft: '1%'}, "slow");
-  $( "#setrentalleng-end" ).fadeIn( "3000", function() {});
-  // setRentalEndDate();
-}
 
 function setRentalEndDate(){
-  endDate = document.getElementById("endDate").value;
-  if(endDate == null || endDate == "" || isNaN(endDate)){
+  rentalTime = document.getElementById("rentalTime").value;
+  if(rentalTime == null || rentalTime == "" || isNaN(rentalTime)){
     alert("Whoops! You have entered in a value which is not what we want! Try again.")
   };
-  if(endDate){
+  if(rentalTime){
       confirmClient()
   }
 }
 
 function confirmClient(){
   document.title = "Everything look good?";
-  $( "#setrentalleng-end" ).fadeOut( "20000", function() {});
+  $( "#setrentalleng" ).fadeOut( "20000", function() {});
   $("#clientconfirm").animate({marginLeft: '1%'}, "slow");
   $( "#clientconfirm" ).fadeIn( "3000", function() {});
   document.getElementById("clname").innerHTML = clientName; 
   document.getElementById("clientseats").innerHTML = clientSeatSelc; 
-  document.getElementById("clientrentaldays").innerHTML = endDate - startDate; 
+  document.getElementById("clientrentaldays").innerHTML = rentalTime; 
 }
 
 function confirmed(){
@@ -112,37 +105,31 @@ function confirmed(){
   function processInfomation(){
     $( "#pleasewait" ).fadeIn( "3000", function() {});
     $("#pleasewait").animate({marginLeft: '1%'}, "slow");
-
       try{
         // Tries the first attempt at finding a car that suits the infomation given
         selectedcar = allCars.find(element => element.seats == clientSeatSelc);
         //  if the number of seats that is lower than the higher amount of seats (9) it will find something greater than the amount given ot if there was a mistake, equal too.
         if($.inArray(clientSeatSelc, allCars.seats)){
           selectedcar = allCars.find(element => element.seats >= clientSeatSelc && element.amountinstock > 1);
-          noSeats(true);
-        }else if($.inArray(clientSeatSelc, allCars.seats)){
-          selectedcar = allCars.find(element => element.seats >= clientSeatSelc && element.amountinstock > 1);
-          noSeats(true);
         }else if(selectedcar.amountinstock < 1){
           selectedcar = allCars.find(element => element.amountinstock > 1 && element.seats >= clientSeatSelc);
-          noStock(true);
          // Fixing an error I didn't think about :< this makes sure that we are giving them a car that suits there needs.
          if(selectedcar.seats < clientSeatSelc){
               selectedcar = allCars.find(element => element.seats > clientSeatSelc);
-              noSeats(true);
          }
-     }    // if($.inArray(Math.max(allCars), allCars.seats)){
-        //  selectedcar = allCars.find(element => element.seats < clientSeatSelc);
-      //}
+     }  
+      /* if($.inArray(Math.max(allCars), allCars.seats)){
+          selectedcar = allCars.find(element => element.seats < clientSeatSelc);
+         }  
+      */
+
         // checking if it could actually find a car that has the right amount of seats.
         // If the best car is not in stock lets find the next best thing
         if(selectedcar.amountinstock < 1){
              selectedcar = allCars.find(element => element.amountinstock > 1 && element.seats >= clientSeatSelc);
-             noStock(true);
             // Fixing an error I didn't think about :< this makes sure that we are giving them a car that suits there needs.
             if(selectedcar.seats < clientSeatSelc){
                  selectedcar = allCars.find(element => element.seats > clientSeatSelc);
-                 noSeats(true);
             }
         }
         // Only console log once we are sure that we are happy with our choice.
@@ -153,8 +140,6 @@ function confirmed(){
         console.log("We encounted an error, want to try again?" + "\n" + err, 'background: #222; color: #bada55')
     }
     try{
-      // This gives the amount of time away
-      var rentalTime = endDate - startDate;
       // Price is given for the day by charging the rentaltime and doubling it 2
       var rentalTimePrice = rentalTime * 2;
       /* We can now process this all together else
@@ -168,23 +153,13 @@ function confirmed(){
     }
   }
 
-  function noStock(stoc){
-    if(stoc){
-      $( "#nostock" ).fadeIn( "3000", function() {});
-    }
-  }
-  function noSeats(seat){
-    if(seat){
-      $( "#noseats" ).fadeIn( "3000", function() {});
-    }
-  }
 
   function informClient(){
     document.title = "We have chosen you " + selectedcar.name + " woohoo!"
     // Using JQuery UI once again to create a nice fade into the new section
     $( "#pleasewait" ).fadeOut( "3000", function() {});
-    $('body').animate({backgroundColor: 'white'}, 'slow');
     $("#selectedcar").animate({marginLeft: '1%'}, "slow");
+    $('body').animate({backgroundColor: 'white'}, 'slow');
     $( "#selectedcar" ).fadeIn( "3000", function() {});
     // Fill in the HTML with the infomation given.
     document.getElementById("carname").innerHTML = selectedcar.name;
@@ -193,3 +168,13 @@ function confirmed(){
     document.getElementById("rentalprice").innerHTML = finalPrice;
 
   } 
+
+  function finalDetails(){
+    $( "#selectedcar" ).fadeOut( "3000", function() {});
+    $('body').animate({backgroundColor: 'blue'}, 'slow');
+    $("#selectedcar").animate({marginLeft: '1%'}, "slow");
+    $( "#redirectingnotice" ).fadeOut( "3000", function() {});
+    window.location.href = '/Users/maxwebb/Documents/Digital%20Technology/Javascript/javascript-robscarrental/finaldetails.html?car = ' + selectedcar + '?clientname = ' + clientName + '?clientrentaldetails = {"price": ' + rentalTimePrice + ', "rentalTime": ' + rentalTime + '}';
+
+
+  }
