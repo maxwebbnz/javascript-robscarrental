151353b7591daf789243e33f340f33be6dd68193
Max Webb <60947369+maxwebbnz@users.noreply.github.com>
2020-08-17 21:03:49 +1200

    v.0.3.1
    Finished all main logic
    
    THIS IS A BUGGY RELEASE!!!
    
    Please do not expect this to run as normal, this may function weirdly!!!
    
    -mw

--- manifest/app.js
+++ manifest/app.js
@@ -11,18 +11,24 @@ var clientName;
 var clientSeatSelc;
 var rentalTime;
 var rentalTimePrice;
-var selectedcar;
+var selectedCar;
 var finalPrice;
 var clientEmail;
 // New Variables (For v2)
 var usersPath;
 var userIdealCar;
+var userPriceRange;
 
 //  Regex's for name and numeric values vaildation
 
-const nameReg = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
+// textReg just checks for letters and white space, it won't pass numbers
+const textReg = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
+// numReg only passes numbers, nothing else + characters
 const numReg = /^\d+$/;
-
+// designed to check for emails
+const emailReg = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+// this checks for white space, and a proper email address!
+const nameReg= /^([a-zA-Z]{2,}\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\s?([a-zA-Z]{1,})?)/;
 
 // Common names that are entered as fake
 const commonNames = {}
@@ -33,14 +39,14 @@ const rentalFee = 4.50;
 
 // Base Car defintions (names, etc)
     // All cars with 1-3 Seats (classifed as small cars on the site)
-    var bacMono = {"name": "BAC Mono", "price": 1, "seats": 1, "amountinstock": 2, "imgurl": "https://thedriven.io/wp-content/uploads/2019/09/electric-mini-cooper-se-P90357228_highRes.jpg"};
-    var mx5 = {"name": "Fazada MX5","price": 2, "seats": "2", "amountinstock": 0, "imgurl": "https://thedriven.io/wp-content/uploads/2019/09/electric-mini-cooper-se-P90357228_highRes.jpg"};
+    var bacMono = {"name": "BAC Mono", "price": 40, "seats": 1, "amountinstock": 2, "imgurl": "https://thedriven.io/wp-content/uploads/2019/09/electric-mini-cooper-se-P90357228_highRes.jpg"};
+    var mx5 = {"name": "Fazada MX5","price": 230, "seats": "2", "amountinstock": 0, "imgurl": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Mazda_Roadster_%28MX-5%29_by_Negawa_Bridge_%28cropped%29.jpg/1200px-Mazda_Roadster_%28MX-5%29_by_Negawa_Bridge_%28cropped%29.jpg"};
     // All Cars with 4-6 Seats (classifed as family cars on the site)
-    var mini = {"name": "The Mini","price": 3, "seats": "4", "amountinstock": 2, "imgurl": "https://thedriven.io/wp-content/uploads/2019/09/electric-mini-cooper-se-P90357228_highRes.jpg"};
-    var falcon = {"name": "Yord Falcon","price": 4, "seats": "5", "amountinstock": 2};
+    var mini = {"name": "The Mini","price": 100, "seats": "4", "amountinstock": 2, "imgurl": "https://thedriven.io/wp-content/uploads/2019/09/electric-mini-cooper-se-P90357228_highRes.jpg"};
+    var falcon = {"name": "Yord Falcon","price": 120, "seats": "5", "amountinstock": 2};
     // All Cars with 7+ Seats (classifed as extra large cars on the site)
-    var outLander = {"name": "Yoyota OutLander","price": 4, "seats": "7", "amountinstock": 2};
-    var estima = {"name": "Yoyota Estima","price": 4, "seats": "8", "amountinstock": 2};
+    var outLander = {"name": "Yoyota OutLander","price": 140, "seats": "7", "amountinstock": 2};
+    var estima = {"name": "Yoyota Estima","price": 180, "seats": "8", "amountinstock": 2};
 
 // All Cars on lot
     const allCars = [bacMono, mx5, mini, falcon, outLander, estima];   
--- manifest/homepage.js
+++ manifest/homepage.js
@@ -13,6 +13,7 @@ Some content referenced in this file can be found in the app file (app.js)
 var global = {
   setSection: function (section) {
     document.title = "Rob's Rentals | " + section
+    // Log for error checking / handling
     console.log("Section Changed to " + section)
   }
 }
@@ -22,7 +23,7 @@ var validate = {
   // This is used to vaildate all input in the program, referenced by vaildate.(whateveryouwanttovaildate)
   text: function (str) {
     // Text vaildation
-    if (nameReg.test(str)) {
+    if (textReg.test(str)) {
       // If string parsed through matches the nameReg-ex
       return true
       // return true
@@ -32,18 +33,43 @@ var validate = {
       // return false
     }
   },
-  num: function (str) {
-    if (numReg.test(str)) {
+  nameSpecfic: function (str) {
+    if (nameReg.test(str)) {
+      // If string parsed through matches the nameReg-ex
       return true
+      // return true
+    } else {
+      // else if it does not match
+      return false
+      // return false    
+    }
+  },
+  num: function (str) {
+    // Num vaildation
+    if (numReg.test(str)) {
+      // If string parsed through matches the numReg-ex
+      return true
+      // return true
+    } else {
+      // else if does not match
+      return false
+      // return false
+    }
+  },
+  email: function (str) {
+    if (emailReg.test(str)) {
+      return true;
     } else {
       return false
     }
   }
-};
+}
+
 
 // End of Global Declerations \\ 
 // Welcome Section Loader \\
 $(document).ready(function () {
+  // run the homePage.display function to display the first content shown on the page
   homePage.display();
 });
 
@@ -57,10 +83,47 @@ var homePage = {
   },
   destroy: function () {
     $(".headers").fadeOut();
+    // included the final section for destroying the whole section!
+    $(".final").fadeOut();
     console.log("Home Page Destroyed")
+  },
+  // ran when the program is finished so we can loop back to main home page
+  finish: function () {
+    $(".final").fadeIn();
+    global.setSection("All Done!")
   }
 }
 
+
+// Inserting getting the client's name here!
+var nameFinder = {
+  display: function () {
+    $(".nameFinder").fadeIn();
+    global.setSection("Your Name")
+  },
+  destroy: function () {
+    $(".nameFinder").fadeOut();
+    // Log for error checking / handling
+    console.log("Name Entry Page Destroyed")
+  },
+  submit: function () {
+    if (!validate.nameSpecfic(document.getElementById("clientName").value)) {
+      // show the error message under the input box
+      $(".alert").fadeIn();
+    } else {
+      // Set clientName to the value of the clientName input
+      clientName = document.getElementById("clientName").value;
+      // Destroy this section
+      this.destroy()
+      // Move on to the next section
+      menuSelector.display();
+      // Fade in the userName content for the navbar.
+      $(".userName").fadeIn();
+      // Fill in every userName content with the users name on the page
+      document.getElementById("userName").innerHTML = clientName;
+    }
+  }
+}
 // Menu Selector
 var menuSelector = {
   display: function () {
@@ -74,9 +137,11 @@ var menuSelector = {
   },
   destroy: function () {
     $(".selector").fadeOut();
+    // Log for error checking / handling
     console.log("Menu Selection Page Destroyed")
   }
 }
+
 // Enter Details for both views
 var enterDetails = {
   display: function () {
@@ -87,39 +152,231 @@ var enterDetails = {
       // Fade in the .filter div class.
       $('.filter').fadeIn();
       // Else if the user wants to use our automatic process (v1 of program)
-    } else if (userPath == "auto") {
+    } if (userPath == "auto") {
       // Change name for section
       global.setSection("Automatic Choice")
       // Fade in the .auto-start div class (Start of process).
-      $('.auto-start').fadeIn();
+      this.autoAllocation("seats")
+    }
+  },
+  // This function below is used for destroying every part of this section
+  destroy: function (section) {
+    // if the section the program wants to delete is the filter
+    if (section == "filter") {
+      // fade out the filter class
+      $(".filter").fadeOut();
+      // Log for error checking / handling
+      console.log("All Parts from the Filter Selection Page Destroyed")
+      // else if the section the program wants to delete is the auto section
+    } else if (section == "auto") {
+      // fade out the auto class
+      menuSelector.destroy();
+      $(".auto").fadeOut();
+      $(".price-range").fadeOut();
+      $(".seatsselection").fadeOut();
+      // Log for error checking / handling.
+      console.log("All Parts from the Auto Selection Page Destroyed")
     }
   },
   // The functions below are used for when the user is wanting to find their ideal car
-  idealCar: function () {
-    if (validate.text(document.getElementById("carIdealForClient").value)) {
+  idealCar: function (_usrErr) {
+    // if the text entered into the input of carIdealForClient matches the nameRegex (vaildate func)
+    // if the user actually didn't know what they wanted
+    if (_usrErr == 'noCar') {
+      userPath = "auto";
+      this.destroy("filter");
+      this.display();
+      // log 
+      console.log("User made an Error! Whoops! Changing Section to " + userPath)
+    } else {
+      // set the userIdealCar to the value of the inoput
       userIdealCar = document.getElementById("carIdealForClient").value;
-      console.log("hello that is true man")
-      this.destroy("filter")
-    } else if (!validate.text(document.getElementById("carIdealForClient").value)) {
-      $("#carIdealForClient").after('<p class="alert">Are you sure that is correct infomation? Because we dont think so</p>');
-      console.log("Error! Incorrect Infomation Given")
+      // enterDetails.destroy('filter')
+      this.destroy("filter");
+      // Then find the car!
+      // Fixed the issue by running it inside of the if statement, I think I am qutie clever haha!
+      if (findCar("filter", userIdealCar)) {
+        // log success
+        console.log("findCar() returned true, conveying infomation to final steps.")
+        this.destroy();
+        selected.display();
+        // Filling in site span's with infomation from selectedCar object
+        document.getElementById("carName").innerHTML = selectedCar.name;
+        document.getElementById("carImage").src = selectedCar.imgurl;
+        document.getElementById("carPrice").innerHTML = selectedCar.price;
+
+      } else {
+        // log failure
+        console.log("findCar() didn't find a car, that sucks :(")
+        // Now we want to tell the user what we are doing
+        alert("We couldn't find you a car that matches your wishes, hence why we are now moving you to the automatic")
+        userPath = "auto";
+        this.destroy("filter");
+        this.display();
+        console.log("Program made an Error! Whoops! Changing Section to " + userPath)
+      }
     }
   },
-  destroy: function (section) {
-    if (section == "filter") {
-      $(".filter").fadeOut();
-      console.log("All Parts from the Filter Selection Page Destroyed")
-    } else if (section == "auto") {
-      $(".auto").fadeOut();
-      console.log("All Parts from the Auto Selection Page Destroyed")
+  // a looping sort of function which loops back around to auto allocation (geez max make it hard on yourself)
+  setSection: function (_sec) {
+    this.autoAllocation(_sec);
+  },
+  // This is used for us finding the car for them
+  // section stands for what part of the progress we are up to (only used once)
+  autoAllocation: function (_section) {
+    if (_section == "seats") {
+      $(".auto").fadeIn();
+      $(".seatsselection").fadeIn();
+      $('#seats').change(function () {
+        clientSeatSelc = $(this).val();
+        console.log("Seat Selection has been set to " + clientSeatSelc);
+        // this.autoAllocation("price")
+        // instead...
+        this.setSection("price");
+        this.destroy();
+      })
+    }
+    if (_section == "price") {
+      $(".seatsselection").fadeOut();
+      $(".price-range").fadeIn();
+      $('#b1, #b2, #b3').click(function () {
+        userPriceRange = $(this).val();
+        console.log(userPriceRange + " is the price range the user wants")
+        this.setSection("processInfomation");
+        this.destroy();
+      });
+    }
+    // the fun happens now, processing all this manual entries
+    if (_section == "processInfomation") {
+      try {
+        if (userPriceRange == "budget") {
+          selectedCar = allCars.find(element => element.seats >= clientSeatSelc);
+          if (!selectedCar.price < 100) {
+            selectedCar = allCars.find(element => element.price <= 100 && element.seats >= clientSeatSelc);
+            if (selectedCar == null) {
+              alert("couldn't find a car in this price range")
+              this.destroy();
+              menuSelector.display();
+            }
+          }
+        } else if (userPriceRange == "family") {
+          selectedCar = allCars.find(element => element.seats >= clientSeatSelc);
+          if (!selectedCar.price < 200) {
+            selectedCar = allCars.find(element => element.price <= 200 && element.seats >= clientSeatSelc);
+            if (selectedCar == null) {
+              alert("couldn't find a car in this price range")
+              this.destroy();
+              menuSelector.display();
+            }
+          }
+        } else if (userPriceRange == "professional") {
+          selectedCar = allCars.find(element => element.seats >= clientSeatSelc);
+          if (!selectedCar.price < 300) {
+            selectedCar = allCars.find(element => element.price <= 300 && element.seats >= clientSeatSelc);
+            if (selectedCar == null) {
+              alert("couldn't find a car in this price range")
+              this.destroy();
+              menuSelector.display();
+            }
+          }
+        }
+        // This only works if there is no errors
+        document.getElementById("carName").innerHTML = selectedCar.name;
+        document.getElementById("carImage").src = selectedCar.imgurl;
+        document.getElementById("carPrice").innerHTML = selectedCar.price;
+        this.destroy();
+        selected.display();
+      }
+      catch (err) {
+        console.log(err)
+        alert("Sorry! \n There was an error with something, sending you to the start")
+        this.destroy();
+        menuSelector.display();
+      }
+    }
+  }
+};
+
+// Show Selected Section Objects
+var selected = {
+  display: function () {
+    $(".showSelected").fadeIn();
+    global.setSection("Chosen a " + selectedCar.name)
+  },
+  destroy: function () {
+    $(".showSelected").fadeOut();
+    // Log for error checking / handling
+    console.log("Selected Page Destroyed")
+  },
+  happy: function (_happy) {
+    if (_happy) {
+      console.log("Client has locked in question A")
+      this.destroy();
+      email.display();
+    } else {
+      alert("You weren't happy, so we are taking you back around to the start");
+      this.destroy();
+      menuSelector.display();
     }
   }
 }
 
+// Email Section Object
+var email = {
+  display: function () {
+    $(".emailPlease").fadeIn();
+    global.setSection("Email")
+  },
+  destroy: function () {
+    $(".emailPlease").fadeOut();
+    // Log for error checking / handling
+    console.log("Selected Page Destroyed");
+  },
+  submit: function () {
+    // Check that the email passes the vaildation test
+    // if it is not a vaild email
+    if (!validate.email(clientEmail = document.getElementById("emailAddress").value)) {
+      // show the error message under the input box
+      $(".alert").fadeIn();
+      // else if the email is vaild
+    } else {
+      // move onto the next section
+      clientEmail = document.getElementById("emailAddress").value;
+      this.destroy();
+      this.sendEmail();
+      homePage.finish();
+    }
+  },
+  sendEmail: function () {
+    // This code is straight and not revised from v1 (this will be re done in final verison but only here for now ._.)
+    const emailContent = { header: "Rob's Rentals Car Company" };
+    Email.send({
+      SecureToken: "182ea469-28d5-45ae-a220-b314e5e29c77",
+      To: clientEmail,
+      From: "maxwebblighting@gmail.com",
+      Subject: `Robs Rental Company | ${selectedCar.name}`,
+      Body: '<html><head><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"></head><header><h3>' + emailContent.header + '</h3></header><br><h3>Kia Ora ' + clientName + ',<br><b>Here is the car we have selected for you</b><br><table><thead><tr><th>Seats</th></tr></thead><tr><td>' + selectedCar.name + '</td><td>' + selectedCar.seats + '</td></tr></table><br><h3>Your final price comes to $' + finalPrice + '<br>Please any questions please just let us know<br> Thanks, Robs Rentals'
+    }).then(
+      message => alert(message)
+    );
+  }
+}
 
-
-
-
+function findCar(_path, _usrCar) {
+  // We don't need to vailate car names because some might have symbols in them :)
+  if (_path == 'filter') {
+    // Setting the car to the found car 
+    selectedCar = allCars.find(element => element.name == _usrCar)
+    // Now returning true if we have found a car with the infomation given
+    if (selectedCar != null) {
+      return true
+    } else if (selectedCar == null) {
+      return false
+    }
+  } else {
+    console.log("hello i did not work :(")
+  }
+};
 
 
 
